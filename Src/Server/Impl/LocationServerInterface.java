841115a4723785e09778d9ed3f8bd56e9ee91050
CaioAndersonMM <caioandersonmartins@gmail.com>
2025-03-13 12:30:12 -0300

    feat: Adicionada interface do Servidor de Localização

---
+++ Src/Server/Impl/LocationServerInterface.java
@@ -0,0 +1,10 @@
+package Src.Server.Impl;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface LocationServerInterface extends Remote {
+    void registerProxy(String proxyName) throws RemoteException;
+    List<String> getProxies() throws RemoteException;
+}
