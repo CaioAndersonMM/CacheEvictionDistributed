ed44debae4e56e0cb4f97cc8fea3f45360a79e9d
CaioAndersonMM <caioandersonmartins@gmail.com>
2025-03-13 13:25:28 -0300

    feat: Adicionada exceção UnknownHostException ao método receberMensagem e ao método main do ProxyServer

--- Src/Server/Impl/ProxyRMI.java
+++ Src/Server/Impl/ProxyRMI.java
@@ -1,5 +1,6 @@
 package Src.Server.Impl;
 
+import java.net.UnknownHostException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -10,5 +11,5 @@ public interface ProxyRMI extends Remote {
     String verificarStatus() throws RemoteException;
     OrdemServico buscar(int codigobusca) throws RemoteException;
     void notificarNovoProxy(String novoProxy) throws RemoteException;
-    Object receberMensagem(Object message) throws RemoteException;
+    Object receberMensagem(Object message) throws RemoteException, UnknownHostException;
 }
