c77baf79925ed343828469a386ce3ada7abc1c14
CaioAndersonMM <caioandersonmartins@gmail.com>
2025-03-13 14:36:09 -0300

    feat: Adicionada primeira réplica da Proxy

---
+++ Src/Server/ProxyReplica.java
@@ -0,0 +1,15 @@
+package Src.Server;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.RemoteException;
+import Src.Server.Impl.ProxyImpl;
+
+public class ProxyReplica {
+    public static void main(String[] args) throws RemoteException {
+        try {
+            new ProxyImpl(5015, InetAddress.getLocalHost().getHostAddress(), 5055);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
