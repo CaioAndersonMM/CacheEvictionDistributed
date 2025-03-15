58f71c87482346f2063ef98af83bd50fbc818747
CaioAndersonMM <caioandersonmartins@gmail.com>
2025-03-15 19:13:07 -0300

    remove: Método de backup de log da interface de Backup

--- Src/Server/Impl/BackupServerInterface.java
+++ Src/Server/Impl/BackupServerInterface.java
@@ -7,5 +7,4 @@ import Src.OrdemServico;
 
 public interface BackupServerInterface extends Remote {
     boolean backupDatabase(Object databaseContent, OrdemServico os) throws RemoteException;
-    void backupLog(String logContent) throws RemoteException;
 }
